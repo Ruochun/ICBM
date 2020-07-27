ea91b146489a9f048a4504111dda89b43c12a443
Ruochun <ruochunz@gmail.com>
2020-07-26 19:10:07 -0500

    arguments added; first publishable version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ICBM
 Ice Cream Ball Maker
+
+It makes overlapping incecream balls that fill up a mesh.
+
+To use it, first install Pybullet:
+pip3 install pybullet --upgrade --user
+
+Then call the script, something like this (a bear.obj demo is included):
+python3 test_decomp.py -i bear.obj -o result.csv -c parts.obj -v 1000000 -r 0.6
+
+
--- test_decomp.py
+++ test_decomp.py
@@ -1,19 +1,30 @@
 import pybullet as p
-import pybullet_data as pd
+#import pybullet_data as pd
 import os
 import wavefront as wf
 import util
 import numpy as np
+import argparse, sys
 
-csv_out = "result.csv"
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=
+        argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--input_obj", "-i", type=str, dest="input_obj", default="mesh.obj", help="File name of the .obj input")
+parser.add_argument("--output_csv", "-o", type=str, dest="output_csv", default="result.csv", help="File name of the .csv output")
+parser.add_argument("--convex_obj", "-c", type=str, dest="convex_obj", default="parts.obj", help="File name of the intermediate .obj convex shapes")
+parser.add_argument("--convex_log", type=str, dest="convex_log", default="log.txt", help="File name of the intermediate convex decomposition logs")
+parser.add_argument("--interpolate_ratio", "-r", type=float, dest="ratio", default=0.5,  
+			help="Must be in [0,1]; set it close to 1 to make the final output spheres large, close to 0 to make them small")
+parser.add_argument("--voxel_resolution", "-v", type=int, dest="voxel_resolution", default=50000,
+			help="The resolution at which the convex decomposition takes place; larger number usually leads to more final spheres")
+args = parser.parse_args(sys.argv[1:])
 
 p.connect(p.DIRECT)
-name_in = "bear.obj"#os.path.join(pd.getDataPath(), "part.obj")
-name_out = "part_vhacd2.obj"
-name_log = "log.txt"
-p.vhacd(name_in, name_out, name_log, concavity=0.0000,alpha=0.04,resolution=500000 )
 
-parts = wf.load_obj(name_out)#, triangulate=True)
+############## IF OTHER CONVEX DECOMPOSITION PARAM NEEDED TO CHANGE: PLEASE DIRECTLY CHANGE THEM HERE #######################
+p.vhacd(args.input_obj, args.convex_obj, args.convex_log, concavity=0.0025, alpha=0.04, resolution=args.voxel_resolution)
+#############################################################################################################################
+
+parts = wf.load_obj(args.convex_obj)#, triangulate=True)
 
 xyzr = np.zeros((len(parts), 4))
 part_id = 0
@@ -30,7 +41,7 @@ for part in parts:
 
     part_id += 1
 
-np.savetxt(csv_out, xyzr, header = "x,y,z,r", delimiter=",")
+np.savetxt(args.output_csv, xyzr, header = "x,y,z,r", delimiter=",")
 
 
 
